e8bbc32fa73cff810d48a6e1c71b808fdf03b1c5
minzhy <50997516+minzhy@users.noreply.github.com>
2022-09-25 21:32:07 +0800

    update
    a

--- clock-in.py
+++ clock-in.py
@@ -75,6 +75,7 @@ class DaKa(object):
 
         try:
             old_infos = re.findall(r'oldInfo: ({[^\n]+})', html)
+            old_infos = old_infos if len(old_infos) != 0 else re.findall(r'def = ({[^\n]+})', html)
             if len(old_infos) != 0:
                 old_info = json.loads(old_infos[0])
             else:
